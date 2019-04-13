da72acfd8aada61463c89e6da4f10f14ee81cdee
Nicola Regattieri <nicregattieri@gmail.com>
2019-04-13 20:13:24 -0300

    completed shelf

--- src/js/react/Shelf.js
+++ src/js/react/Shelf.js
@@ -3,25 +3,40 @@ import Product from "../../../JSON/product";
 
 class Shelf extends Component {
   render() {
-    var settings = {
-      dots: true,
-      infinite: true,
-      slidesToShow: 2,
-      slidesToScroll: 1
-    };
     return (
       <div className="shelf">
-        <div className="shelf__title">Produtos</div>
-        <div className="shelf__wrapper carousel" data-glide-el="track">
+        <div className="container">
+          <h3 className="shelf__title tlt-txt">Produtos</h3>
+        </div>
+        <div className="shelf__wrapper shelf-carousel" data-glide-el="track">
           {Product.map((productDetail, index) => {
             return (
               <li key={productDetail.productId}>
                 <div className="product">
-                  <h1 className="product__name">{productDetail.name}</h1>
                   <img
                     className="product__media"
                     src={productDetail.skus[0].image}
                   />
+                  <h1 className="product__name">{productDetail.name}</h1>
+                  <img className="product__rating" src="/arquivos/rating.png" />
+                  <div className="product__price">
+                    <p className="price__list sml-txt">
+                      De: {productDetail.skus[0].listPriceFormated}
+                    </p>
+                    <p className="price__best">
+                      por {productDetail.skus[0].bestPriceFormated}
+                    </p>
+                    <p className="price__installment sml-txt">
+                      ou em {productDetail.skus[0].installments}x de{" "}
+                      {productDetail.skus[0].installmentsValue}
+                    </p>
+                  </div>
+                  <div className="product__actions">
+                    <button className="btn btn--buy">
+                      <img src="/arquivos/add-cart.png" />
+                      Comprar
+                    </button>
+                  </div>
                 </div>
               </li>
             );
